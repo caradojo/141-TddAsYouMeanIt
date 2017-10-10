ca418dac97d0c3893f1c659b67dc70f70c960b73
Mathieu Cans <mathieucans@gmail.com>
2017-10-10 12:39:03 +0200

    init

---
+++ TddAsIfYouMeanIt/TicTacToeShould.cs
@@ -0,0 +1,15 @@
+﻿    using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddAsIfYouMeanIt
+{
+    [TestClass]
+    public class TicTacToeShould
+    {
+        [TestMethod]
+        public void noWinnerAtBegining()
+        {
+            Assert.IsFalse(true);
+        }
+    }
+}
---
+++ doc/TDD as if you mean it.mm
@@ -0,0 +1,26 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1507631396027" ID="ID_500049412" MODIFIED="1507631479623" TEXT="TDD as if you mean it">
+<node CREATED="1507631497250" ID="ID_1455359253" MODIFIED="1507631503513" POSITION="right" TEXT="Remove duplication &#xe0; partir de 3"/>
+<node CREATED="1507631780849" ID="ID_1295362851" MODIFIED="1507631783859" POSITION="left" TEXT="premier test">
+<node CREATED="1507631785271" ID="ID_1793543938" MODIFIED="1507631794023" TEXT="d&apos;abord une constante">
+<node CREATED="1507631794639" ID="ID_888770277" MODIFIED="1507631817431">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      noWinnerAtBeginning
+    </p>
+    <p>
+      assert.IsTrue(false)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
