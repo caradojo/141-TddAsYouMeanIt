abb7342b7b085ae5f7fa5e36dd91afbb600d1edf
Mathieu Cans <mathieucans@gmail.com>
2017-10-10 18:07:06 +0200

    séparation des classes

---
+++ TddAsIfYouMeanIt/TicTacToe.cs
@@ -0,0 +1,32 @@
+﻿namespace TddAsIfYouMeanIt
+{
+    public class TicTacToe
+    {
+        public static string CreateGame(string game)
+        {
+            return game;
+        }
+
+        public static bool HasWinner(string game)
+        {
+            return HasOneRowOfCross(game) 
+                   || HasOneDiagOfCross(game)
+                   || HasColDiagOfCross(game);
+        }
+
+        private static bool HasColDiagOfCross(string game)
+        {
+            return "x--"+ "xoo" + "x--" == game;
+        }
+
+        private static bool HasOneDiagOfCross(string game)
+        {
+            return "x--"+ "oxo" + "--x" == game;
+        }
+
+        private static bool HasOneRowOfCross(string game)
+        {
+            return game.IndexOf("xxx") % 3 == 0;
+        }
+    }
+}
