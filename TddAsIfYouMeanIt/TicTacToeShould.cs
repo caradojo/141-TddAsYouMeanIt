8b35bf38eae2e0243eb9f6131221b96efa006d79
Mathieu Cans <mathieucans@gmail.com>
2017-10-10 13:39:47 +0200

    green

--- TddAsIfYouMeanIt/TicTacToeShould.cs
+++ TddAsIfYouMeanIt/TicTacToeShould.cs
@@ -10,49 +10,21 @@ namespace TddAsIfYouMeanIt
         [TestMethod]
         public void noWinnerAtBegining()
         {
-            var game = CreateGame("---"
+            var game = TicTacToe.CreateGame("---"
                        + "---"
                        + "---");
-            var hasWinner = HasWinner(game);
+            var hasWinner = TicTacToe.HasWinner(game);
             Assert.IsFalse(hasWinner);
         }
-
-        private static string CreateGame(string game)
-        {
-            return game;
-        }
-
-        private static bool HasWinner(string game)
-        {
-            return HasOneRowOfCross(game) 
-                || HasOneDiagOfCross(game)
-                || HasColDiagOfCross(game);
-        }
-
-        private static bool HasColDiagOfCross(string game)
-        {
-            return "x--"+ "xoo" + "x--" == game;
-        }
-
-        private static bool HasOneDiagOfCross(string game)
-        {
-            return "x--"+ "oxo" + "--x" == game;
-        }
-
-        private static bool HasOneRowOfCross(string game)
-        {
-            return game.IndexOf("xxx") % 3 == 0;
-        }
-
 
 
         [TestMethod]
         public void noWinnerAfterOneMove()
         {
-            var game = CreateGame("x--"
+            var game = TicTacToe.CreateGame("x--"
                        + "---"
                        + "---");
-            var hasWinner = HasWinner(game);
+            var hasWinner = TicTacToe.HasWinner(game);
             var nbMove = 1;
             Assert.IsFalse(hasWinner && nbMove == 1);
         }
@@ -60,60 +32,60 @@ namespace TddAsIfYouMeanIt
         [TestMethod]
         public void oneLineOfCrossHasWinner()
         {
-            var game = CreateGame("xxx"
+            var game = TicTacToe.CreateGame("xxx"
                        + "oo-"
                        + "---");
-            var hasWinner = HasWinner(game);
+            var hasWinner = TicTacToe.HasWinner(game);
             Assert.IsTrue(hasWinner);
         }
 
         [TestMethod]
         public void secondLineOfCrossHasWinner()
         {
-            var game = CreateGame("oo-" 
+            var game = TicTacToe.CreateGame("oo-" 
                        + "xxx"
                        + "---");
-            var hasWinner = HasWinner(game);
+            var hasWinner = TicTacToe.HasWinner(game);
             Assert.IsTrue(hasWinner);
         }
 
         [TestMethod]
         public void thridLineOfCrossHasWinner()
         {
-            var game = CreateGame("oo-"
+            var game = TicTacToe.CreateGame("oo-"
                        + "---"
                        + "xxx");
-            var hasWinner = HasWinner(game);
+            var hasWinner = TicTacToe.HasWinner(game);
             Assert.IsTrue(hasWinner);
         }
 
         [TestMethod]
         public void fiveMoveHasNoWinner()
         {
-            var game = CreateGame("oo-"
+            var game = TicTacToe.CreateGame("oo-"
                        + "--x"
                        + "xx-");
-            var hasWinner = HasWinner(game);
+            var hasWinner = TicTacToe.HasWinner(game);
             Assert.IsFalse(hasWinner);
         }
 
         [TestMethod]
         public void oneDiagOfCrossHasWinner()
         {
-            var game = CreateGame("x--"
+            var game = TicTacToe.CreateGame("x--"
                        + "oxo"
                        + "--x");
-            var hasWinner = HasWinner(game);
+            var hasWinner = TicTacToe.HasWinner(game);
             Assert.IsTrue(hasWinner);
         }
 
         [TestMethod]
         public void oneColOfCrossHasWinner()
         {
-            var game = CreateGame("x--"
+            var game = TicTacToe.CreateGame("x--"
                        + "xoo"
                        + "x--");
-            var hasWinner = HasWinner(game);
+            var hasWinner = TicTacToe.HasWinner(game);
             Assert.IsTrue(hasWinner);
         }
     }
