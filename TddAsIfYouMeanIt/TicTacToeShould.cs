dde233e030d4f828f518cc251a8b5f809be68872
Mathieu Cans <mathieucans@gmail.com>
2017-10-24 13:20:52 +0200

    greenphase with duplication

--- TddAsIfYouMeanIt/TicTacToeShould.cs
+++ TddAsIfYouMeanIt/TicTacToeShould.cs
@@ -25,6 +25,29 @@ namespace TddAsIfYouMeanIt
             Assert.IsFalse(hasWinner());
         }
 
+        [TestMethod]
+        public void has_winner_for_three_crosses_on_first_line()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void has_winner_for_three_crosses_on_second_line()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void has_winner_for_three_crosses_on_third_line()
+        {
+            Assert.IsTrue(true);
+        }
+
+        
+
+
+
+
         private static bool hasWinner()
         {
             return false;
